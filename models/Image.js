3f09e38642e69f62ae0dbf8754da5b144adab4ed
Achmad Akbar Megantara <achmegantara@gmail.com>
2021-04-24 14:25:16 +0700

    full project server staycation

---
+++ models/Image.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+const mongoose = require("mongoose");
+
+const imageSchema = new mongoose.Schema({
+  imageUrl: {
+    type: String,
+    required: true
+  }
+})
+
+module.exports = mongoose.model('Image', imageSchema)
